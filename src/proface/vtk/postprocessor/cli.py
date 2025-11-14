4474cea31b34d7839a5e53b83750729bc7d3706f
Stefano Miccoli <stefano.miccoli@polimi.it>
2025-11-14 18:25:23 +0100

    Refactor: implement Mesh class with methods for incidences renumbering

--- src/proface/vtk/postprocessor/cli.py
+++ src/proface/vtk/postprocessor/cli.py
@@ -4,11 +4,13 @@
 
 
 import dataclasses
+import itertools
 import os
 
-import h5py
-import meshio
+import h5py  # type: ignore[import-untyped]
+import meshio  # type: ignore[import-untyped]
 import numpy as np
+import numpy.typing as npt
 import tyro
 
 from proface.vtk.postprocessor import topotable
@@ -20,31 +22,66 @@ class Config:
     out: os.PathLike  # output VTU file (.vtu)
 
 
-def main() -> None:
+class Mesh:
+    """Container for ProFACE mesh object, as saved in neutral h5 format"""
+
+    def __init__(self, h5: h5py.File) -> None:
+        self.points: npt.NDArray[np.float32] = np.asarray(
+            h5["nodes"]["coordinates"], dtype=np.float32
+        )
+        self.point_ids: npt.NDArray[np.int32] = np.asarray(
+            h5["nodes"]["numbers"], dtype=np.int32
+        )
+        assert len(self.points) == len(self.point_ids)
+        assert np.all(self.point_ids[:-1] < self.point_ids[1:]), (
+            "point ids are not strictly sorted"
+        )
+
+        self.cells: list[tuple[str, npt.NDArray[np.int32]]] = []
+        for abq_topo, dataset in h5["elements"].items():
+            self.cells.append(
+                (
+                    topotable[abq_topo],
+                    dataset["incidences"].astype(np.int32)[()],
+                )
+            )
+
+        self._to_zerobased()
+
+    @property
+    def n_points(self) -> int:
+        return len(self.points)
+
+    def _to_zerobased(self) -> None:
+        z_num = np.arange(self.n_points, dtype=np.int32)
+        delta = self.point_ids - z_num
+        if np.all(delta == delta[0]):
+            for _, c in self.cells:
+                c -= delta[0]
+        else:
+            assert np.all(delta[:-1] <= delta[1:])
+            uv, uc = np.unique_counts(delta)
+            assert np.all(uv[:-1] < uv[1:])
+            # here we assume that uc is smallish
+            for d, (a, b) in zip(
+                uv,
+                itertools.pairwise(itertools.accumulate(uc, initial=0)),
+                strict=True,
+            ):
+                for _, c in self.cells:
+                    c[
+                        (c >= self.point_ids[a]) & (c <= self.point_ids[b - 1])
+                    ] -= d
+
+
+def main() -> int:
     config = tyro.cli(Config)
 
-    cells = []
-    cell_data = {}
-
     with h5py.File(config.fea) as h5:
-        points = h5["nodes"]["coordinates"].astype(np.float32)[()]
-        numbering = h5["nodes"]["numbers"][()]
-        for abq_topo, dataset in h5["elements"].items():
-            cb = meshio.CellBlock(
-                cell_type=topotable[abq_topo],
-                data=dataset["incidences"].astype(np.int32)[()],
-            )
-            cells.append(cb)
+        inmesh = Mesh(h5)
 
-    znum = np.arange(len(numbering), dtype=np.int32)
-    delta = numbering[0]
-    if np.all(numbering - znum == delta):
-        for cb in cells:
-            cb.data -= delta
-    else:
-        msg = "Non consecutive node numbering"
-        raise NotImplementedError(msg)
-
-    mesh = meshio.Mesh(points=points, cells=cells, cell_data=cell_data)
-    mesh.write(config.out, binary=True, compression="zlib")
+    mesh = meshio.Mesh(points=inmesh.points, cells=inmesh.cells, cell_data={})
+    mesh.write(config.out, file_format="vtu", binary=True, compression="zlib")
     print(config.out)
+
+    return 0
