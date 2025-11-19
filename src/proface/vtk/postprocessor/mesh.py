4e05f1f3bbefdd1776a9a33e47ef634795fa8f8d
Stefano Miccoli <stefano.miccoli@polimi.it>
2025-11-19 14:19:33 +0100

    Correct typo

--- src/proface/vtk/postprocessor/mesh.py
+++ src/proface/vtk/postprocessor/mesh.py
@@ -15,7 +15,7 @@ topotable = {
     "C3D4": "tetra",
     "C3D5": "pyramid",
     "C3D6": "wedge",
-    "C3D8": "hexaedron",
+    "C3D8": "hexahedron",
     "C3D10": "tetra10",
     "C3D15": "wedge15",
     "C3D20": "hexahedron20",
