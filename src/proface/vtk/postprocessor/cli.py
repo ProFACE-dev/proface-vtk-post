1d105751e42aff85e362489ea7e2c578d11febd4
Stefano Miccoli <stefano.miccoli@polimi.it>
2026-05-05 16:39:44 +0200

    Implement logging with rich.logging.RichHandler

--- src/proface/vtk/postprocessor/cli.py
+++ src/proface/vtk/postprocessor/cli.py
@@ -4,15 +4,28 @@
 
 import argparse
 import dataclasses
+import enum
+import logging
 import pathlib
-import sys
 
 import h5py  # type: ignore[import-untyped]
 import meshio  # type: ignore[import-untyped]
 import tyro
+from rich.console import Console
+from rich.logging import RichHandler
 
 from proface.vtk.postprocessor import __version__
 from proface.vtk.postprocessor.mesh import Mesh, topotable
+
+
+class Loglevel(enum.Enum):
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
@@ -37,6 +50,9 @@ class Config:
     save_fea_results: bool = False
     """save FEA results as cell and point data"""
 
+    log_level: Loglevel = Loglevel.WARNING
+    """logging verbosity"""
+
 
 def main() -> int:
     parser = argparse.ArgumentParser(add_help=False)
@@ -46,9 +62,23 @@ def main() -> int:
     _, remaining = parser.parse_known_args()
 
     config = tyro.cli(Config, args=remaining)
+    logging.basicConfig(
+        level=config.log_level.value,
+        format="%(message)s",
+        handlers=[
+            RichHandler(
+                console=Console(stderr=True),
+                rich_tracebacks=True,
+                show_path=False,
+                log_time_format="%Y-%m-%d %H:%M:%S",
+            )
+        ],
+    )
 
+    logger.info("\N{BLACK RIGHT-POINTING SMALL TRIANGLE} Translation started")
     try:
         with h5py.File(config.fea) as h5:
+            logger.info("Parsing %s", config.fea)
             inmesh = Mesh(
                 h5,
                 load_elsets=config.save_elsets,
@@ -57,19 +87,21 @@ def main() -> int:
             if config.save_fea_results:
                 inmesh.load_fea_results(h5)
     except (OSError, ValueError) as err:
-        print(f"Unable to parse FEA file '{config.fea}'", file=sys.stderr)
-        print(err, file=sys.stderr)
+        logger.error("Unable to parse FEA file '%s'", config.fea)  # noqa: TRY400
+        logger.error("%s", err)  # noqa: TRY400
         return 1
 
     if config.pfa is not None:
         try:
             with h5py.File(config.pfa) as h5:
+                logger.info("Parsing %s", config.pfa)
                 inmesh.load_results(h5)
         except (OSError, ValueError) as err:
-            print(f"Unable to parse PfA file '{config.pfa}'", file=sys.stderr)
-            print(err, file=sys.stderr)
+            logger.error("Unable to parse PfA file '%s'", config.pfa)  # noqa: TRY400
+            logger.error("%s", err)  # noqa: TRY400
             return 1
 
+    logger.info("Creating meshio data structure")
     mesh = meshio.Mesh(
         points=inmesh.points,
         cells=[
@@ -82,7 +114,10 @@ def main() -> int:
         cell_data=inmesh.cell_data,
         point_data=inmesh.point_data,
     )
+    logger.info("%s", str(mesh))
+    logger.info("Writing %s", config.out)
     mesh.write(config.out, file_format="vtu", binary=True, compression="zlib")
+    logger.info("\N{CHECK MARK} Translation completed")
     print(config.out)
 
     return 0
